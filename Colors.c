297865829240aff53257448a72e44b5b604562c2
atlas-research <redlight_bandit@proton.com>
2022-06-11 22:52:34 -0700

    Colors package for C

---
+++ Colors.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define KNPM "\x1B[0m" 
+#define KRED "\x1B[1;31m" 
+#define KGRN "\x1B[1;32m" 
+#define KYEL "\x1B[1;33m" 
+#define KBLU "\x1B[1;34m" 
+#define KMAG "\x1B[1;35m" 
+#define KCYN "\x1B[1;36m"
+#define KWHT "\x1B[1;37m"
+
+#define BKNRM "\x1B[0m" 
+#define BKRED "\x1B[1;31m" 
+#define BKGRN "\x1B[1;32m" 
+#define BKYEL "\x1B[1;33m" 
+#define BKBLU "\x1B[1;34m" 
+#define BKMAG "\x1B[1;35m" 
+#define BKCYN "\x1B[1;36m"
+#define BKWHT "\x1B[1;37m"
+
+int main()
+{
+    printf("%sred\n", KRED);
+    printf("%sgreen\n", KGRN);
+    printf("%syellow\n", KYEL);
+    printf("%sblue\n", KBLU);
+    printf("%ssmagenta\n", KMAG);
+    printf("%scyan\n", KCYN);
+    printf("%swhite\n", KWHT);
+   
+    printf("%snormal\n", BKNRM);
+    printf("%sred\n", BKRED);
+    printf("%sgreen\n", BKGRN);
+    printf("%syellow\n", BKYEL);
+    printf("%sblue\n", BKBLU);
+    printf("%smagenta\n", BKMAG);
+    printf("%scyan\n", BKCYN);
+    printf("%swhite\n", BKWHT);
+    printf("%snormal\n", BKNRM);
+    return 0;
+}
+
